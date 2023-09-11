d71eb07e98298214c9821a9b91c2c26049acbea0
Ianwilliams123456 <143635806+Ianwilliams123456@users.noreply.github.com>
2023-09-11 15:27:17 -0700

    adding dates to every part of the function

--- Powershell Project 1.ps1
+++ Powershell Project 1.ps1
@@ -14,5 +14,8 @@ $Date = (Get-date).AddDays(-$daysbefore)
 Get-AdComputer -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $computerloginpath
 
 $ObjectExportPath = 'desktop\Other_Objects_list.csv'
+$objectloginpath = 'desktop\objectlogin_list.csv'
 Get-AdObject -Filter * | Where-Object {($_.Objectclass -notlike "*user*") -and ($_.Objectclass -notlike "*computer*")} | Select-Object DistinguishedName, Name, ObjectClass |  Export-Csv -NoType $ObjectExportPath
-
+$daysbefore = read-host "how many days to look back?"
+$Date = (Get-date).AddDays(-$daysbefore)
+Get-AdComputer -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $computerloginpath

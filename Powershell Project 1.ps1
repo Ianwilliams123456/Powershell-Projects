5c695fb7b6f50d5e40b2fda2f6440180d1767a6f
Ianwilliams123456 <143635806+Ianwilliams123456@users.noreply.github.com>
2023-09-11 15:30:13 -0700

    added naming stuff to adobjects

--- Powershell Project 1.ps1
+++ Powershell Project 1.ps1
@@ -18,4 +18,4 @@ $objectloginpath = 'desktop\objectlogin_list.csv'
 Get-AdObject -Filter * | Where-Object {($_.Objectclass -notlike "*user*") -and ($_.Objectclass -notlike "*computer*")} | Select-Object DistinguishedName, Name, ObjectClass |  Export-Csv -NoType $ObjectExportPath
 $daysbefore = read-host "how many days to look back?"
 $Date = (Get-date).AddDays(-$daysbefore)
-Get-AdComputer -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $computerloginpath
+Get-AdObject -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $objectloginpath

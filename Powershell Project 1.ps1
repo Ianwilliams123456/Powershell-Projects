dec52c20aa7536dfaf2172600b6bd9d9a6b469b8
Ianwilliams123456 <143635806+Ianwilliams123456@users.noreply.github.com>
2023-09-11 14:04:51 -0700

    second commit several functioning options

--- Powershell Project 1.ps1
+++ Powershell Project 1.ps1
@@ -1,4 +1,14 @@
+$UserExportPath = 'desktop\Users_list.csv’
+$userloginpath = 'desktop\userlogin_list.csv'
+Get-ADUser -Filter * | Select-object DistinguishedName,Name,UserPrincipalName,lastlogondate | Export-Csv -NoType $UserExportPath
+$daysbefore = read-host "how many days to look back?"
+$Date = (Get-date).AddDays(-$daysbefore)
+get-aduser -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $userloginpath
 
 
-Get-EventLog -logname   system -newest 3
+$ComputerExportPath = 'desktop\Computers_list.csv'
+Get-AdComputer -Filter * | Select-Object DistinguishedName,Name,Ipv4address | Export-Csv -NoType $ComputerExportPath
+
+$ObjectExportPath = 'desktop\Other_Objects_list.csv'
+Get-AdObject -Filter * | Where-Object {($_.Objectclass -notlike "*user*") -and ($_.Objectclass -notlike "*computer*")} | Select-Object DistinguishedName, Name, ObjectClass |  Export-Csv -NoType $ObjectExportPath
 

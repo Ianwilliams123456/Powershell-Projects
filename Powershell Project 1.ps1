8f6eacb0693738afe28d26ed9ef429f988a60327
Ianwilliams123456 <143635806+Ianwilliams123456@users.noreply.github.com>
2023-09-11 15:05:40 -0700

    edited a bit on adcomputer Lines

--- Powershell Project 1.ps1
+++ Powershell Project 1.ps1
@@ -7,7 +7,11 @@ get-aduser -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | expo
 
 
 $ComputerExportPath = 'desktop\Computers_list.csv'
+$computerloginpath = 'desktop\computerlogin_list.csv'
 Get-AdComputer -Filter * | Select-Object DistinguishedName,Name,Ipv4address | Export-Csv -NoType $ComputerExportPath
+$daysbefore = read-host "how many days to look back?"
+$Date = (Get-date).AddDays(-$daysbefore)
+Get-AdComputer -Filter {(Enabled -eq "false") -and (lastlogondate -lt $Date)} | export-csv -notype $computerloginpath
 
 $ObjectExportPath = 'desktop\Other_Objects_list.csv'
 Get-AdObject -Filter * | Where-Object {($_.Objectclass -notlike "*user*") -and ($_.Objectclass -notlike "*computer*")} | Select-Object DistinguishedName, Name, ObjectClass |  Export-Csv -NoType $ObjectExportPath
